3fe5c1f29485d639671b2d04e81857a7fd979091
Shreyash Ghanekar <shreyasghanekar35@gmail.com>
2025-08-09 16:53:52 +0530

    Deployment: updated BASE_URL(server) at frontend

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,2 @@
-export const BASE_URL = "http://localhost:8080/api";
-// export const BASE_URL = "https://resume-builder-zkad.onrender.com/api"
-//export const BASE_URL = "https://resume-builder-mern-one.vercel.app/api"
+// export const BASE_URL = "http://localhost:8080/api";
+export const BASE_URL = "https://resume-builder-6dzw.onrender.com"
